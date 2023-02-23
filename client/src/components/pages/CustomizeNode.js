508360e58a8b6b772e6334022099ca074884eb22
lrjimenez-2000 <lrjimenez@miners.utep.edu>
2023-02-22 19:58:56 -0700

    Took off the input for these two pages

--- client/src/components/pages/CustomizeNode.js
+++ client/src/components/pages/CustomizeNode.js
@@ -21,33 +21,6 @@ function CustomizeNode(){
 
     return (
         <form onSubmit={handleBack}>
-          <h3>Node Color</h3>
-          <h3>Modify Node Image</h3>
-          <h3>Specify Node Status</h3>
-          
-          <input
-            type="text"
-            name="fileInput"
-            placeholder="Node Color"
-            onChange={handleChange}
-            
-          ></input>
-
-          <input
-            type="text"
-            name="fileInput"
-            placeholder="Modify Node Image"
-            onChange={handleChange}
-          ></input>  
-
-          <input
-            type="text"
-            name="fileInput"
-            placeholder="Specify Node Status"
-            onChange={handleChange}
-          ></input>
-
-
           <button type="submit">Back</button>
           <button type="submit">Node Color</button>
           <button type="submit">Modify Node Image</button>
