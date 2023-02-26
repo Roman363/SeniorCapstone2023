4cb46247616f9a9b3d7510a12dc728d7780d2a1f
Alexis Rodriguez <ralexis1092@gmail.com>
2023-02-25 17:43:46 -0700

    Reader Set-Up

--- server/reader.py
+++ server/reader.py
@@ -2,14 +2,20 @@
 import tarfile
 
 #Opens up the rollup as a tarfile object
-file = tarfile.open(r"C:\Users\ralex\Downloads\20221027-144534-SQ.tar.gz")
+file = tarfile.open("C:\Users\ralex\Downloads\20221027-144534-SQ.tar.gz")
 #Prints list of files inside ot the tarfile object
-#file.list()
+file.list()
 
 #returns list of files as tarinfo objects
-files = file.getmembers()
+#files = file.getmembers("canvasmaps")
 
-print(files)
+#print(files)
+
+#file.extractall()
+
+files = file.getmember("data/canvasmaps")
+print(files.isdir())
+
 
 #closes the tarfile
 file.close()
