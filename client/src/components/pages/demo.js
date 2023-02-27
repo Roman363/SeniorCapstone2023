9079e24847de89f57780aa79908b949d10e9dc0c
Jay-UTEP <JayPark-UTEP>
2023-02-26 19:55:40 -0700

    table feature is added in Demo.js

--- client/src/components/pages/demo.js
+++ client/src/components/pages/demo.js
@@ -7,16 +7,13 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+// name, calories, fat, carbs, protein 
+function createData(systemIP, associations, op, activeService, protocol, sentPackets, trafficAmount) {
+  return { systemIP, associations, op, activeService, protocol, sentPackets, trafficAmount };
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+const statistics = [
+  createData("157.143.80.158", "Open", "Windows", "Windows Bot Service", "VTP", "350", "Low")
 ];
 
 export default function BasicTable() {
@@ -25,26 +22,30 @@ export default function BasicTable() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>SystemIP</TableCell>
+            <TableCell align="right">Associations</TableCell>
+            <TableCell align="right">OP&nbsp;(g)</TableCell>
+            <TableCell align="right">Active Service&nbsp;(g)</TableCell>
+            <TableCell align="right">Protocol&nbsp;(g)</TableCell>
+            <TableCell align="right">Sent Packets&nbsp;(g)</TableCell>
+            <TableCell align="right">Traffic Amount&nbsp;(g)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {statistics.map((row) => (
             <TableRow
               key={row.name}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.systemIP}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.associations}</TableCell>
+              <TableCell align="right">{row.op}</TableCell>
+              <TableCell align="right">{row.activeService}</TableCell>
+              <TableCell align="right">{row.protocol}</TableCell>
+              <TableCell align="right">{row.sentPackets}</TableCell>
+              <TableCell align="right">{row.trafficAmount}</TableCell>
             </TableRow>
           ))}
         </TableBody>
