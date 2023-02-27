618d586277aae03beb84e728aa39d0eee6ee22e8
Roman363 <sotorom363@gmail.com>
2023-02-27 13:13:22 -0700

    Changes to be committed: 	modified:   client/src/App.js 	modified:   client/src/components/pages/NetworkMap.js

--- client/src/components/pages/NetworkMap.js
+++ client/src/components/pages/NetworkMap.js
@@ -7,9 +7,6 @@ function NetworkMap() {
     <h3>NetworkMap</h3>
     <iframe title="myframe" src="https://charts.mongodb.com/charts-team-6-sun-city-software-rsomw/embed/charts?id=63eb0411-505d-4876-887c-f52740c9916f&maxDataAge=3600&theme=dark&autoRefresh=true"    >
       </iframe></div>
-      
-    
-    
   )
 }
 
