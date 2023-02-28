1344d21c6a28eac737629bab5f8ae879a4193fca
Roman363 <sotorom363@gmail.com>
2023-02-27 20:13:11 -0700

    Finalized any missing errors for front

--- client/src/components/pages/dashBoard/MainDashBoard.js
+++ client/src/components/pages/dashBoard/MainDashBoard.js
@@ -5,7 +5,7 @@ import Select from "react-select";
 
 export default function MainDashBoard() {
   const navigate = useNavigate();
-
+  
   function handleMainDashBoard(e) {
     e.preventDefault();
     navigate("/MainDash");
@@ -39,29 +39,34 @@ export default function MainDashBoard() {
   }
 
   return (
-    <form id="mainDashBoard">
-      <h3>NAIVE</h3>
-      <h5>File Name</h5>
+    <body>
+  <header id="mainNav">
+      <h2 id="naive">Naive</h2>
+      <button id="cyberVA">Cyber VA</button> 
+      <button id="assessmentDash">Assessment Dashboard</button>
+  </header>
 
-      <button onClick={handleMainDashBoard}>Main Dashboard</button>
-      <button onClick={handleCyberdashBoard}>Cyber VA Dashboard</button>
-      <button onClick={handleAssessmentDashBoard}>Assessment Dashboard</button>
 
-      {/* menubar */}
-      <Select
-        options={options}
-        onChange={handleMenu}
-      />
-      <iframe
-        title="myframe"
-        src="https://charts.mongodb.com/charts-team-6-sun-city-software-rsomw/embed/charts?id=63eb0411-505d-4876-887c-f52740c9916f&maxDataAge=3600&theme=dark&autoRefresh=true"
-        height="500" width="60%" 
-      ></iframe>
-      <br />
+  <form>
+    <div class="content-container">
+        <div class="main">
 
-      <button onClick={handleAddNode}>Add Node</button>
-      <button>Settings</button>
-      <button>Quit</button>
-    </form>
+            <label>
+                Select Project
+                <br />
+            </label>
+
+        </div>
+    </div>
+</form>
+
+    <div id="footer">
+      <button id="quit">Quit</button> 
+      <button id="settings">Settings</button>
+      <button id="addNode">Add Node</button>
+      <button id="deleteNode">Delete Node</button>
+      {/* <!-- onclick={handleQuit} --> */}
+    </div>
+</body>
   );
 }
--- client/src/components/pages/home/Home.js
+++ client/src/components/pages/home/Home.js
@@ -15,27 +15,39 @@ function Home() {
   }
 
   return (
-<body>
-  <header id="mainNav">
-      <h2 id="naive">Naive</h2>
-      <button id="cyberVA">Cyber VA</button> 
-      <button id="assessmentDash">Assessment Dashboard</button>
-  </header>
+    <body>
+    <header id="mainNav">
+        <h2 id="naive">Naive</h2>
+        {/* <a href="" id="cyberVA">Cyber VA</a> Change this when doing the template part 2 with assessment dashboard and VA Dashboard
+        <a href="" id="assessmentDash">Assessment Dashboard</a> */}
+    </header>
 
 
-  <form method="get">
-      <div id="App">
-        
+    <form method="get">
+        <div id="App">
+          <div class="box-container">
+            <div class="button-container">
+              <div class="content-container">
+                <label>Please enter user initials:</label>
+                <input
+                  type="text"
+                  id="UserInitial"
+                  placeholder="Ex: J.D"
+                ></input>
+                <button onClick={handleCreate}>Create Project</button>
+                <button onClick={handleOpen}>Open Project</button>
+                <button onClick={handleDelete}>Delete Project</button>
+              </div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </form>
+
+      <div id="footer">
+        <button id="quit">Quit</button> 
+        {/* onclick={handleQuit} */}
       </div>
-    </form>
-
-    <div id="footer">
-      <button id="quit">Quit</button> 
-      <button id="settings">Settings</button>
-      <button id="addNode">Add Node</button>
-      <button id="deleteNode">Delete Node</button>
-      
-    </div>
 </body>
   );
 }
